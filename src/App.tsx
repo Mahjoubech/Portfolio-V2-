6b038030cf89ec6a802dd754416da9c5d919ca13
Mahjoubech <charkaouielmahjoub50@gmail.com>
2026-03-17 14:21:36 +0000

    first commmit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <div className="min-h-screen flex items-center justify-center bg-red-200 text-white">
+      <h1 className="text-4xl font-bold text-sky-400 bg-amber-200">
+        hhhhhhhhhhhhhhhhhhhhhhhhhh
+      </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
